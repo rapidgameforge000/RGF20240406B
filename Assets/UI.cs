a2208e66aa9617412b51ab3df9e41a8bec9ae15a
rgf005 <tcagame@tca.ac.jp>
2024-04-06 13:48:27 +0900

    もっこみ

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    private TextMeshProUGUI _score_text;
+    [SerializeField] private TextMeshProUGUI _score_text;
     private int _score;
 
 

27bdd7ae684697a495e09cd08544c92d72333a14
rgf008 <tcagame@tca.ac.jp>
2024-04-06 12:58:14 +0900

    ブロック並べるところまで

--- Assets/Field.cs
+++ Assets/Field.cs
@@ -1,17 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Field : MonoBehaviour
 {
     private UnityEngine.GameObject _object;
+
+    private const int BLOCKNUM = 20;
+    UnityEngine.GameObject BlockPrefab;
+    private UnityEngine.GameObject[] BlockArray;
     // Start is called before the first frame update
     void Start()
     {
 
-        UnityEngine.GameObject prefab = UnityEngine.Resources.Load<UnityEngine.GameObject>("Assets/Resources/Block.prefab");
-        UnityEngine.GameObject instance = UnityEngine.Object.Instantiate(prefab, new Vector3(0,0,0), Quaternion.identity);
-        _object = instance;
+        // リソース読み込み
+        BlockPrefab = UnityEngine.Resources.Load<UnityEngine.GameObject>("Block");
+
+        // UnityEngine.GameObject instance = UnityEngine.Object.Instantiate(BlockPrefab, new Vector3(500,-500,0), Quaternion.identity);
+        //_object = instance;
+
+        // 配列準備
+        BlockArray = new UnityEngine.GameObject[BLOCKNUM];
+
+        int BlockSetUpY = -500;
+        int BlockSetUpStartX = -500;
+        int BlockSizeX = 60;
+        for (int i = 0; i < BLOCKNUM; i++)
+        {
+            UnityEngine.GameObject instance = UnityEngine.Object.Instantiate(BlockPrefab, new Vector3(BlockSetUpStartX + BlockSizeX * i, BlockSetUpY, 0), Quaternion.identity);
+            BlockArray[i] = instance;
+        }
     }
 
     // Update is called once per frame

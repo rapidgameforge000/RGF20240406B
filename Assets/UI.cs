d3e6947fcfeab68dcbe6bb15fffaee4dc7a38dfa
rgf005 <tcagame@tca.ac.jp>
2024-04-06 13:01:09 +0900

    TMP

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UI : MonoBehaviour
 {
+    public TextMeshPro text;
     // Start is called before the first frame update
     void Start()
     {
-        
+     //   text.text = "ooooo";
     }
 
     // Update is called once per frame

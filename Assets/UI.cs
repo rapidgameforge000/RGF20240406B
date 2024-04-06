3f8e0ac970dd340967fd4c3b7ebc49121b3ddc6a
rgf005 <tcagame@tca.ac.jp>
2024-04-06 13:19:43 +0900

    文字追加

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    public TextMeshPro text;
+    public TextMeshProUGUI text;
     // Start is called before the first frame update
     void Start()
     {
-     //   text.text = "ooooo";
+        text.text = "ooooo";
     }
 
     // Update is called once per frame

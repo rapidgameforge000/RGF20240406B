ffbcea08ca9936ec740804e249f24eca32a19d50
rgf005 <tcagame@tca.ac.jp>
2024-04-06 13:26:18 +0900

    数字に変更

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    public TextMeshProUGUI text;
-    // Start is called before the first frame update
+    public TextMeshProUGUI _score;
+
     void Start()
     {
-        text.text = "ooooo";
+        _score.text = "ooooo";
     }
 
-    // Update is called once per frame
+    float aaa = 0.0f;
+
     void Update()
     {
-        
+        int i = 0;
+
+        aaa += Time.deltaTime;
+        if (2.0f < aaa ) {
+            i++;
+            aaa = 0.0f;
+        }
+        _score.text = i.ToString();
     }
 }

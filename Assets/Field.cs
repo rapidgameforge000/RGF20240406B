bc2ab815119057ff7ee4889063516888c8bd256b
rgf008 <tcagame@tca.ac.jp>
2024-04-06 12:36:57 +0900

    tesut　ブロックとFieldのスクリプト

--- Assets/Field.cs
+++ Assets/Field.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Field : MonoBehaviour
 {
+    private UnityEngine.GameObject _object;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        UnityEngine.GameObject prefab = UnityEngine.Resources.Load<UnityEngine.GameObject>("Assets/Resources/Block.prefab");
+        UnityEngine.GameObject instance = UnityEngine.Object.Instantiate(prefab, new Vector3(0,0,0), Quaternion.identity);
+        _object = instance;
     }
 
     // Update is called once per frame
